968c18e0e4865bb27db5ccb9c1b7ad046821f036
jgutierrez <jose19.26@hotmail.com>
2018-06-13 10:05:08 -0500

    Initial commit

---
+++ app/src/androidTest/java/com/example/jjapps/form080/ExampleInstrumentedTest.kt
@@ -0,0 +1,24 @@
+package com.example.jjapps.form080
+
+import android.support.test.InstrumentationRegistry
+import android.support.test.runner.AndroidJUnit4
+
+import org.junit.Test
+import org.junit.runner.RunWith
+
+import org.junit.Assert.*
+
+/**
+ * Instrumented test, which will execute on an Android device.
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+@RunWith(AndroidJUnit4::class)
+class ExampleInstrumentedTest {
+    @Test
+    fun useAppContext() {
+        // Context of the app under test.
+        val appContext = InstrumentationRegistry.getTargetContext()
+        assertEquals("com.example.jjapps.form080", appContext.packageName)
+    }
+}
---
+++ app/src/main/java/com/example/jjapps/form080/MainActivity.kt
@@ -0,0 +1,182 @@
+package com.example.jjapps.form080
+
+import android.graphics.Typeface
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.support.v7.app.AlertDialog
+import android.support.v7.widget.Toolbar
+import android.view.View
+import android.widget.EditText
+import android.widget.TextView
+import com.android.volley.DefaultRetryPolicy
+import com.android.volley.Request
+import com.android.volley.Response
+import com.android.volley.toolbox.StringRequest
+import com.example.jjapps.form080.utils.FontManager
+import com.example.jjapps.form080.utils.GeneralHelper
+import org.json.JSONArray
+import android.view.LayoutInflater
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Button
+import com.airbnb.lottie.LottieAnimationView
+import kotlinx.android.synthetic.main.activity_main.*
+import com.example.jjapps.form080.anim.AnimationEditText
+import com.example.jjapps.form080.utils.VolleySingleton
+
+
+class MainActivity : AppCompatActivity() {
+
+     private lateinit var search: EditText
+    private lateinit var iconPerson: TextView
+    private lateinit var lottieAnimationView:LottieAnimationView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        search = findViewById(R.id.edt_search)
+        iconPerson = findViewById(R.id.icon_person)
+        lottieAnimationView = findViewById(R.id.anim_loading)
+
+        val typeface = Typeface.createFromAsset(this.assets, FontManager.getRuteFASolid())
+        iconPerson.setTypeface(typeface)
+        iconPerson.text = FontManager.fontUser
+
+        activityCreateToolbar()
+
+    }
+
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        menuInflater.inflate(R.menu.info_menu, menu)
+
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        val id = item.itemId
+        return if (id == R.id.menu_info) {
+            GeneralHelper.simpleAlerts(this,"Acerca de","Esta app esta desarollodado por Grupo JJ APPS \n\n Contacto: example@gmail.com" )
+            true
+        } else super.onOptionsItemSelected(item)
+    }
+
+    // title: createToolbar
+    // description: generar un menu personalizado con botones a la derecha para el toolbar.
+    // programmer: jGutierrez
+    private fun activityCreateToolbar() {
+        val mToolbar = this.findViewById<View>(R.id.toolbar) as Toolbar
+        mToolbar.setTitle(getString(R.string.app_name))
+        mToolbar.inflateMenu(R.menu.info_menu)
+        setSupportActionBar(mToolbar)
+
+    }
+
+
+    fun clickSearch(view:View){
+        var ID: String = edt_search.text.toString()
+        var isID:Boolean = GeneralHelper.validateIdPanama(ID)
+        if(isID){
+         getResult(ID)
+        }
+        else
+            AnimationEditText.animateVibration(search,this)
+    }
+
+    //--------------------------------- User download data -------------------------------------------------------------//
+    private fun getResult(ID: String) {
+
+        val link = "http://planillaws.azurewebsites.net/api/user/load/$ID"
+
+        val postRequest = object : StringRequest(Request.Method.GET, link,
+                Response.Listener { response ->
+                    //      Log.d("Response", response);
+                    if (response != "" && response != "null") {
+                        try {
+
+                            val array = JSONArray(response)
+                            var progressCount: Double = 0.0
+                            if (array.length() > 0) {
+                                        val `object` = array.getJSONObject(0)
+                                        var resp: String = `object`.getString("cedula")
+
+                                lottieAnimationView.visibility = View.GONE
+
+                                        if(!resp.equals("0"))
+                                        dialog(`object`.getString("nombre"),`object`.getString("apellido"),`object`.getString("cedula"),`object`.getString("salario"),`object`.getString("cargo"),`object`.getString("planilla"))
+                                        else
+                                            GeneralHelper.simpleAlerts(this,"Alerta","Esta identificación no aparece en la PLANILLA 080")
+
+                            }
+
+                        } catch (ex: Exception) {
+                            lottieAnimationView.visibility = View.GONE
+                          GeneralHelper.simpleAlerts(this,"Error","Lo sentimos, ocurrio un error :(")
+                        }
+
+
+                    }
+                },
+                Response.ErrorListener { error ->
+                    lottieAnimationView.visibility = View.GONE
+                    GeneralHelper.simpleAlerts(this,"Error","Lo sentimos, ocurrio un error :(")
+                }
+        ) {
+
+            override fun getBodyContentType(): String {
+                return super.getBodyContentType()
+            }
+        }
+
+        postRequest.retryPolicy = DefaultRetryPolicy(
+                40000,
+                DefaultRetryPolicy.DEFAULT_MAX_RETRIES,
+                DefaultRetryPolicy.DEFAULT_BACKOFF_MULT)
+              VolleySingleton.getInstance(applicationContext).addToRequestque(postRequest)
+    }
+
+
+    // title: dialogComment
+    // description: presenta informacion del web service .
+    // programmer: jGutierrez
+    private fun dialog(nombreDL:String, apellidoDL: String, cedulaDL: String, salarioDL: String, cargoDL:String, planillaDL: String) {
+        val factory = LayoutInflater.from(this)
+        val deleteDialogView = factory.inflate(R.layout.dialoginfo, null)
+        val deleteDialog = AlertDialog.Builder(this, R.style.DialogTheme).create()
+        deleteDialog.setTitle("Identificación encontrada en PLANILLA 080")
+        val nombre:TextView = deleteDialogView.findViewById(R.id.tv_name)
+        val cedula:TextView = deleteDialogView.findViewById(R.id.tv_cedula)
+        val salario:TextView = deleteDialogView.findViewById(R.id.tv_salario)
+        val cargo:TextView = deleteDialogView.findViewById(R.id.tv_cargo)
+        val planilla:TextView = deleteDialogView.findViewById(R.id.tv_planilla)
+        val icon:TextView = deleteDialogView.findViewById(R.id.icon_person2)
+
+        val btnOk:Button = deleteDialogView.findViewById(R.id.btn_ok)
+
+        nombre.text = nombreDL + " " + apellidoDL
+        cedula.text = cedulaDL
+        salario.text = salarioDL + " $"
+        cargo.text = cargoDL
+        planilla.text = planillaDL
+
+        val typeface = Typeface.createFromAsset(this.assets, FontManager.getRuteFASolid())
+        icon.setTypeface(typeface)
+
+        icon.text = FontManager.fontUser
+
+        deleteDialog.setView(deleteDialogView)
+        deleteDialog.setCancelable(false)
+
+
+        btnOk.setOnClickListener(View.OnClickListener {
+            //your business logic
+            deleteDialog.dismiss()
+        })
+
+        deleteDialog.show()
+    }
+
+
+
+}
